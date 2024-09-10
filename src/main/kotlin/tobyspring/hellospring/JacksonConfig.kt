4429de0eb10ed55cdaf2d22226ae231a7448ae09
bellCold <whdcks420@gmail.com>
2024-09-10 23:04:56 +0900

    feat: jacksonConfig 추가

---
+++ src/main/kotlin/tobyspring/hellospring/JacksonConfig.kt
@@ -0,0 +1,20 @@
+package tobyspring.hellospring
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class JacksonConfig {
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper().apply {
+            enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+            registerKotlinModule()
+        }
+    }
+}
